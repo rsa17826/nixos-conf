e70ac99d1e90f29b45a5eec0d70e89163fe90b44
Your Name <you@example.com>
2025-12-10 14:59:03 -0500

    a

--- configuration.nix
+++ configuration.nix
@@ -56,7 +56,7 @@ services.keyd = {
         settings = {
           main = {
             capslock = "overload(control, esc)";
-	    numlock = "overload(control, esc)";
+	    numlock = "on";
           };
         };
       };
