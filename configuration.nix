68471331dcc697aa78e5bb91ba29a5d90e7bbdfb
Your Name <you@example.com>
2025-12-16 09:14:34 -0500

    a

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,19 @@ in
       };
     };
   };
+  security.polkit.extraConfig = ''
+  polkit.addRule(function (action, subject) {
+    if (subject.isInGroup("users") &&
+        [
+          "org.freedesktop.login1.reboot",
+          "org.freedesktop.login1.reboot-multiple-sessions",
+          "org.freedesktop.login1.power-off",
+          "org.freedesktop.login1.power-off-multiple-sessions"
+        ].indexOf(action.id) !== -1) {
+      return polkit.Result.YES;
+    }
+  });
+'';   
   # systemd.services.numlock = {
   # description = "Enable NumLock at startup";
   # wantedBy = [ "multi-user.target" ];
