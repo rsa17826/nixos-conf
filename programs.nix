def4bb243bd91d18a948c081cdb5198488b2551e
rsa17826 <rssaromeo@tutamail.com>
2025-12-17 21:43:21 -0500

    Update programs.nix

--- programs.nix
+++ programs.nix
@@ -20,7 +20,7 @@
         vim = "nvim";
         "nix-env" = "echo wrong command";
         clearcache = "nix-collect-garbage";
-        clearallache = "sudo nix-collect-garbage --delete-older-than 3d";
+        clearallcache = "sudo nix-collect-garbage --delete-older-than 3d";
       };
     };
     hyprland = {
