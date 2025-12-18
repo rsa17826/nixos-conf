585e4b20d80874eeb7224eb3a0c310f4c8624650
Your Name <you@example.com>
2025-12-17 21:20:41 -0500

    a

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,7 @@ in
   programs.nix-ld.libraries = with pkgs; [
     # Add any missing dynamic libraries for unpackaged programs
     # here, NOT in environment.systemPackages
+    xlib
   ];
   nix.gc.automatic = true;
   nix.gc.dates = "weekly"; # or "daily"
