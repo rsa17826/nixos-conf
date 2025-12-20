9fd930978899f8e71a143b0c9cd00bb5a503b644
Your Name <you@example.com>
2025-12-20 08:53:50 -0500

    a

--- home/home.nix
+++ home/home.nix
@@ -84,9 +84,9 @@
     #   enable = true;
     # };
     home-manager.enable = true;
-    writeShellScriptBin "nix-env" ''
+    (writeShellScriptBin "nix-env" ''
         echo aaaa
-    '';
+    '')
   };
 
   # Optional, hint Electron apps to use Wayland:
